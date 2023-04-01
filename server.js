a72a4fc9603e7c895c125e967dfcde07a16850aa
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-01 14:44:36 -0400

    99 beers express app is done

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.get('/greeting', (req, res) => {
   res.send('Hello stranger');
 });
 
+
 app.get('/greeting/:name', (req, res) => {
     res.send('Good to see you ' + req.params.name);
   });
@@ -21,6 +22,19 @@ app.get('/magic/:randomQuestion', (req, res) => {
     res.send(req.params.randomQuestion +": " + randomAnswer);
 });
 
+app.get('/:bottles', (req, res) => {
+    req.params.bottles = parseInt(req.params.bottles);
+    let bottles = req.params.bottles;
+    res.send(
+        bottles > 0 ? bottles + ' Bottles of Beer on the wall' + '<br/>' +`<a  href='/${bottles - 1}'>Take One down pass it around</>` : bottles + ' Bottles of Beer on the wall'+ '<br/>' +`<a  href='/${bottles + 99}'>No more beers left on the wall let's go to the store and get 99 more</>`
+        );
+});
+
+
+// app.post('/:bottles', (req, res) => {
+
+// res.redirect('/:bottles');
+// });
 
 app.listen(PORT, () => {
     console.log('listening');
